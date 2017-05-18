fb973989e558877cba9fa50ef08b477c0c0379c6
Alberto Zanella <a.zanella@fbk.eu>
2017-05-18 13:53:55 +0200

    fixed comments

--- addon/installTasks.py
+++ addon/installTasks.py
@@ -1,7 +1,7 @@
 #A part of NonVisual Desktop Access (NVDA)
 #This file is covered by the GNU General Public License.
 #See the file COPYING for more details.
-#Copyright (C) 2016 Alberto Zanella <lapostadialberto@gmail.com>
+#Copyright (C) 2016-2017 Alberto Zanella <lapostadialberto@gmail.com>
 
 
 
@@ -13,19 +13,19 @@ import config
 addonHandler.initTranslation()
 
 
-## Translators: title of the message box that appears when the user is installing the addon and an older version prevents the update to be completed.
+# Translators: title of the message box that appears when the user is installing the addon and an older version prevents the update to be completed.
 oldaddontitle = _('Incompatible version of the addon detected')
 
-## Translators: text of the message box that appears when the user is installing the addon and an older version prevents the update to be completed.
+# Translators: text of the message box that appears when the user is installing the addon and an older version prevents the update to be completed.
 oldaddonmsg = _("""An old, incompatible version of this addon has been detected.
 This prevents the installation to be completed.
 Please uninstall the previous version before proceeding with the Lambda addon setup.
 """)
 
-## Translators: title of the message box that appears when user is installing the addon in NVDA and a "lambda" profile is already present. 
+# Translators: title of the message box that appears when user is installing the addon in NVDA and a "lambda" profile is already present. 
 profilefoundtitle = _('lambda profile already exists')
 
-## Translators: text of the message box that appears when user is installing the addon in NVDA and a "lambda" profile is already present. 
+# Translators: text of the message box that appears when user is installing the addon in NVDA and a "lambda" profile is already present. 
 profilefoundmsg = _("""Another profile named "lambda" is already present in your NVDA configuration. 
 This may prevents the addon on create and configure the lambda profile correctly.
 Please review default options for this addon using the Revert LAMBDA Profile Wizard (pressing nvda+alt+r) while the Lambda Application is active.
