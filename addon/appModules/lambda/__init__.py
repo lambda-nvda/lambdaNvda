70ae184ec35d3a1a4d4ceadbb63f30491e26c546
Noelia Ruiz Martínez <nrm1977@gmail.com>
2017-06-19 21:56:39 +0200

    Initial commit

---
+++ addon/appModules/lambda/__init__.py
@@ -0,0 +1,119 @@
+# -*- coding: UTF-8 -*-
+#Addon for Lambda Math Editor
+#This file is covered by the GNU General Public License.
+#See the file COPYING for more details.
+#Copyright (C) 2016-2017 Alberto Zanella <lapostadialberto@gmail.com>
+
+import braille
+import config
+import speech
+import addonHandler
+import appModuleHandler
+import controlTypes
+from . import lambdaProfileSetup
+from lambdaEdit import LambdaDialogEdit,LambdaMainEditor,LambdaMatrixEdit,LambdaBufferEdit
+from NVDAObjects.window import DisplayModelEditableText
+import wx
+from comtypes import COMError
+
+addonHandler.initTranslation()
+
+confspec = {
+	"brailleFlatMode": "boolean(default=True)",
+}
+config.conf.spec["lambda"] = confspec
+
+class AppModule(appModuleHandler.AppModule):
+# Dialogs which has edit fields like the main Lambda Editor
+	_customDialogsNames = (
+	"TStructureW", #Structure view Window
+	"TFrm_Matrix", #Matrix Window
+	"TFrm_Calculator_Simple", #Calculator 
+	"TFrm_Calculator_Viewer", #CTRL+F9
+	)
+	
+	LAMBDA_SPACE = (
+		u"mezera",
+		u"Leerzeichen",
+		u"space",
+		u"Espacio",
+		u"espace",
+		u"spazio",
+		u"spazio di test",
+		u"Espaço",
+		u"medzera",
+	)
+	
+	def __init__(self, *args, **kwargs):
+		super(AppModule, self).__init__(*args, **kwargs)
+		if  (lambdaProfileSetup.profileExists()) :
+			#We update the table abs path since the user should have a portable version and the path may be different.
+			lambdaProfileSetup.updateTablePath()
+		else: #If a lambda profile doesn't exists:
+			lambdaProfileSetup.createLambdaProfile()
+		lambdaProfileSetup.addBrailleTableToGUI()
+	
+	def chooseNVDAObjectOverlayClasses(self, obj, clsList):
+		if (obj.windowClassName == u'TJvEditor') : #Main editor windows
+			clsList.insert(0, LambdaMainEditor)
+		if  (obj.windowClassName == u'TEdit') and (obj.role == controlTypes.ROLE_EDITABLETEXT) :
+			#Dialogs with enhanced spoken math text
+			try :
+				dialogW = obj.parent.parent
+				if dialogW and dialogW.windowClassName in self._customDialogsNames :
+					clsList.insert(0, LambdaDialogEdit)
+					clsList.remove(DisplayModelEditableText)
+			except : pass
+		if obj.windowClassName == u'TMemo' :
+			dialogW = obj.parent.parent
+			if dialogW and dialogW.windowClassName == "TFrm_BufferW" :
+				clsList.insert(0, LambdaBufferEdit)
+		if obj.windowClassName == u'TLambdaEdit' :
+		#Matrix dialog
+			try :
+				dialogW = obj.parent.parent
+				if dialogW and dialogW.windowClassName in self._customDialogsNames :
+					clsList.insert(0, LambdaMatrixEdit)
+					clsList.remove(DisplayModelEditableText)
+			except : pass
+	
+	#report input symbols general handler
+	shouldValueChangeSpeak = False
+	def reportLastInsertedText(self,obj,reason=None) :
+		obj.detectPossibleSelectionChange()
+		cfg = config.conf['lambda']['brailleFlatMode']
+		if not cfg :
+			braille.handler.handleGainFocus(obj)
+		try :
+			s = obj.getLambdaObj().getlastinsertedel(obj.windowHandle, 1)
+		except COMError : s = None
+		if s == None or len(s) == 0 : return
+		if s in self.LAMBDA_SPACE: return
+		if s == u" ": return
+		self.shouldValueChangeSpeak = False
+		if config.conf['keyboard']['speakTypedCharacters']:
+			speech.speakText(s)
+
+	def force_symbol_speak(self) :
+		self.shouldValueChangeSpeak = True
+	
+	def event_valueChange(self,obj,nh) :
+		if isinstance(obj,LambdaMainEditor):
+			if self.shouldValueChangeSpeak :
+				self.reportLastInsertedText(obj,"value")
+		return nh()
+	
+	def terminate(self) :
+		super(AppModule, self).terminate()
+		#Clean-up custom braille tables
+		lambdaProfileSetup.removeBrailleTableToGUI()
+	
+	def script_openQuckProfileWizard(self, gesture):
+		wx.CallAfter(lambdaProfileSetup.onQuickProfileWizardDialog, None)
+	script_openQuckProfileWizard.category = "Lambda"
+	# Translators: Message presented in input help mode.
+	script_openQuckProfileWizard.__doc__ = _("Shows a dialog to revert lambda profile options to the default.")
+	
+	__gestures = {
+	'kb:nvda+alt+r': 'openQuckProfileWizard',
+	}
---
+++ addon/appModules/lambda/lambdaProfileSetup.py
@@ -0,0 +1,139 @@
+#Addon for Lambda Math Editor
+#This file is covered by the GNU General Public License.
+#See the file COPYING for more details.
+#Copyright (C) 2016-2017 Alberto Zanella <lapostadialberto@gmail.com>
+
+import config
+import os
+import globalVars
+import braille
+import addonHandler
+import sharedMessages as shMsg
+import gui
+from gui.settingsDialogs import SettingsDialog
+import wx
+
+addonHandler.initTranslation()
+
+PROFILE_NAME = "lambda"
+SIXDOTS_APP = "app:sixdots"
+
+#Translators: This string is the file name of the Lambda braille table for the translated language. The file should be present in the "brailleTables" directory in this addon. The default is the italian braille translation table.
+TABLE_NAME = _("lambda-ita.utb")
+
+
+# Check whether the lambda profile already exists
+def profileExists() :
+	try :
+		(config.conf._getProfile(PROFILE_NAME))
+		return True
+	except :
+		return False
+
+'''
+Creates a new profile lambda.ini. By default it is of the form:
+
+[braille]
+	translationTable = table\path\tableName
+	tetherTo = focus
+	readByParagraph = False
+
+[lambda]
+	brailleFlatMode = True
+'''
+def createLambdaProfile() :
+	config.conf.createProfile(PROFILE_NAME)
+	config.conf.save()
+	lp = config.conf._getProfile(PROFILE_NAME,True)
+	#Creates braille config
+	brlcfg = {}
+	setDefaultBraillevalues(brlcfg)
+	#Write profile
+	lp["braille"] = brlcfg
+	lp.write()
+	#Update profile trigger
+	trigs = config.conf.triggersToProfiles["app:"+PROFILE_NAME] = PROFILE_NAME
+	trigs = config.conf.triggersToProfiles[SIXDOTS_APP] = PROFILE_NAME
+	#Update profile configs
+	config.conf.saveProfileTriggers()
+
+
+def setDefaultBraillevalues(brlcfg,translationTable = True,tetherTo = True,readByParagraph = True,wordWrap = True):
+	if translationTable : brlcfg["translationTable"] = _getBrlTablePath(TABLE_NAME)
+	if tetherTo : brlcfg["tetherTo"] = "focus"
+	if readByParagraph : brlcfg["readByParagraph"] = False
+	if wordWrap : brlcfg["wordWrap"]=False
+
+# Adds the braille table to the list in braille settings dialog
+def addBrailleTableToGUI():
+	#Retrieves all tables in addon directory with prefix 'lamda-'
+	newTables = tuple((_getBrlTablePath(file),file,False,) for file in os.listdir(_getBrlTablesDir()) if os.path.isfile(os.path.join(_getBrlTablesDir(), file)) and file.startswith("lambda-"))
+	braille.TABLES = braille.TABLES + newTables
+
+# Removes the braille table to the list in braille settings dialog
+def removeBrailleTableToGUI():
+	braille.TABLES = tuple(table for table in braille.TABLES if not table[1].startswith("lambda-"))
+
+# Retrieves the current absolute path for the braille table and updates the profile entry (useful for portable NVDA)
+def updateTablePath() :
+	lp = config.conf._getProfile(PROFILE_NAME,True)
+	#If a system table has been selected, do not force the abspath
+	if "lambda-" not in lp["braille"]["translationTable"] : return
+	basepath = _getBrlTablesDir()
+	tablename = os.path.basename(lp["braille"]["translationTable"])
+	if basepath not in lp["braille"]["translationTable"]: #Directory has been moved
+		lp["braille"]["translationTable"] = _getBrlTablePath(tablename)
+		lp.write()
+
+# Gets the absolute path of the braille table in tableName
+def _getBrlTablePath(tableName) :
+	return os.path.join(_getBrlTablesDir(),tableName)
+	
+#Get the absolute path to the brailleTables directory
+def _getBrlTablesDir() :
+	return os.path.abspath(os.path.join(globalVars.appArgs.configPath, "addons", "lambda", "appModules",PROFILE_NAME,"brailleTables"))
+
+
+def onQuickProfileWizardDialog(evt) :
+	gui.mainFrame._popupSettingsDialog(QuickProfileWizardDialog)
+
+class QuickProfileWizardDialog(SettingsDialog):
+	# Translators: This is the label for the Quick Profile Wizard dialog.
+	# This dialog helps the user to reset relevant profile options without deleting his custom settings.
+	title = _("Revert LAMBDA Profile Wizard")
+
+	def makeSettings(self, settingsSizer):
+		# Translators: This is the static text of the Quick Profile Wizard dialog.
+		msgIntro=_("Choose which options you want to reset to the default value for the Lambdas profile")
+		self.introStxt=wx.StaticText(self,-1,label=msgIntro)
+		settingsSizer.Add(self.introStxt,flag=wx.BOTTOM)
+		# Translators: This is the label for a checkbox in the
+		# Quick Profile Wizard dialog.
+		self.defaultTranslationTableCheckBox=wx.CheckBox(self,wx.NewId(),label=_("Keep the LAMBDA braille table for the current language (%s)") % TABLE_NAME)
+		self.defaultTranslationTableCheckBox.SetValue(True)
+		settingsSizer.Add(self.defaultTranslationTableCheckBox,border=10,flag=wx.BOTTOM)
+		# Translators: This is the label for a checkbox in the
+		# Quick Profile Wizard dialog.
+		self.brailleTetherToFocusCheckBox=wx.CheckBox(self,wx.NewId(),label=_("Set the braille cursor to tether the focus"))
+		self.brailleTetherToFocusCheckBox.SetValue(True)
+		settingsSizer.Add(self.brailleTetherToFocusCheckBox,border=10,flag=wx.BOTTOM)
+		# Translators: This is the label for a checkbox in the
+		# Quick Profile Wizard dialog.
+		self.disableReadByParagraphCheckBox=wx.CheckBox(self,wx.NewId(),label=_("Disable the Braille reading by paragraph"))
+		self.disableReadByParagraphCheckBox.SetValue(True)
+		settingsSizer.Add(self.disableReadByParagraphCheckBox,border=10,flag=wx.BOTTOM)
+		# Translators: This is the label for a checkbox in the
+		# Quick Profile Wizard dialog.
+		self.disableBrailleWordWrapCheckBox=wx.CheckBox(self,wx.NewId(),label=_("Disable word wrappping of the braille line"))
+		self.disableBrailleWordWrapCheckBox.SetValue(True)
+		settingsSizer.Add(self.disableBrailleWordWrapCheckBox,border=10,flag=wx.BOTTOM)
+
+	def postInit(self):
+		self.defaultTranslationTableCheckBox.SetFocus()
+
+	def onOk(self,evt):
+		lp = config.conf._getProfile(PROFILE_NAME,True)
+		brlcfg = lp["braille"]
+		setDefaultBraillevalues(brlcfg,self.defaultTranslationTableCheckBox.IsChecked(),self.brailleTetherToFocusCheckBox.IsChecked(),self.disableReadByParagraphCheckBox.IsChecked(),self.disableBrailleWordWrapCheckBox.IsChecked())
+		lp.write()
+		super(QuickProfileWizardDialog, self).onOk(evt)
