b3b9936b0b037f648dfa492d3a84863d6ece54f7
a.zanella <a.zanella@fbk.eu>
2023-02-18 15:32:40 +0100

    updated version -- tested against NVDA 2023.1

--- buildVars.py
+++ buildVars.py
@@ -19,7 +19,7 @@ addon_info = {
 	# Translators: Long description to be shown for this add-on on add-on information from add-ons manager
 	"addon_description" : _("This addon provides access to the Lambda math editor with both braille and speech support."),
 	# version
-	"addon_version" : "1.4.1",
+	"addon_version" : "1.4.2",
 	# Author(s)
 	"addon_author" : "Alberto Zanella, Ivan Novegil",
 	# URL for the add-on documentation support
@@ -31,7 +31,7 @@ addon_info = {
 	# Minimum NVDA version supported (e.g. "2018.3")
 	"addon_minimumNVDAVersion" : "2021.1.0",
 	# Last NVDA version supported/tested (e.g. "2018.4", ideally more recent than minimum version)
-	"addon_lastTestedNVDAVersion" : "2022.1.0",
+	"addon_lastTestedNVDAVersion" : "2023.1.0",
 	# Add-on update channel (default is stable or None)
 	"addon_updateChannel" : None,
 }
